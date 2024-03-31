142df2ec82fba2c0353db8358f936a48e748ef1f
bomzae <tmzl235@naver.com>
2024-03-31 22:12:39 +0900

    refactor: 리뷰 작성 API Request(ReviewCreateRequest)에 date 필드 추가

--- server/src/main/java/com/codecozy/server/dto/request/ReviewCreateRequest.java
+++ server/src/main/java/com/codecozy/server/dto/request/ReviewCreateRequest.java
@@ -3,8 +3,8 @@ package com.codecozy.server.dto.request;
 import java.util.List;
 
 public record ReviewCreateRequest (
+        String date,
         String keyword,
         List<Integer> select,
         String comment
-
 ) {}
