aca9e4a793f159ca5ad63ae7b7ba58ab625c131f
bee-p <weileenw@naver.com>
2024-08-22 19:03:11 +0900

    fix: #41에 따른 수정사항
    - 해당 한줄평에 등록된 reviewer 데이터가 있는지 없는지만 판별 후, 해당 한줄평에 등록된 모든 reviewer 정보를 지우도록 수정함

--- server/src/main/java/com/codecozy/server/repository/BookReviewReviewerRepository.java
+++ server/src/main/java/com/codecozy/server/repository/BookReviewReviewerRepository.java
@@ -4,6 +4,7 @@ import com.codecozy.server.composite_key.BookReviewReviewerKey;
 import com.codecozy.server.entity.BookReview;
 import com.codecozy.server.entity.BookReviewReviewer;
 import com.codecozy.server.entity.Member;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookReviewReviewerRepository extends JpaRepository<BookReviewReviewer, BookReviewReviewerKey> {
@@ -11,4 +12,8 @@ public interface BookReviewReviewerRepository extends JpaRepository<BookReviewRe
     // FIXME: 위 수정사항에 따라 findByBookReview 메소드와 연관된 코드들 수정 필요
     BookReviewReviewer findByBookReview(BookReview bookReview);
     BookReviewReviewer findByBookReviewAndMember(BookReview bookReview, Member member);
+
+    // 해당 comment_id에 등록된 모든 reviewer 정보 지우기
+    @Transactional
+    void deleteAllByBookReview(BookReview bookReview);
 }
