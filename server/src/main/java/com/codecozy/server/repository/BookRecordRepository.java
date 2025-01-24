e1b3a028e8549f7c543287ab777efdea5b9644e1
bee-p <weileenw@naver.com>
2025-01-24 13:41:11 +0900

    delete: 사용하지 않는 JPA 메소드 삭제
    - 쿼리 최적화로 인해 더 이상 사용하지 않는 JPA Repository 메소드 삭제함

--- server/src/main/java/com/codecozy/server/repository/BookRecordRepository.java
+++ server/src/main/java/com/codecozy/server/repository/BookRecordRepository.java
@@ -13,9 +13,6 @@ public interface BookRecordRepository extends JpaRepository<BookRecord, BookReco
     // 특정 유저의 특정 책의 독서기록 가져오기
     BookRecord findByMemberAndBook(Member member, Book book);
 
-    // 특정 유저의 모든 독서 노트 가져오기
-    List<BookRecord> findAllByMember(Member member);
-
     // 특정 유저의 특정 독서 상태의 모든 독서 노트 가져오기
     List<BookRecord> findAllByMemberAndReadingStatus(Member member, int readingStatus);
 
--- server/src/main/java/com/codecozy/server/repository/BookReviewReactionRepository.java
+++ server/src/main/java/com/codecozy/server/repository/BookReviewReactionRepository.java
@@ -1,9 +1,7 @@
 package com.codecozy.server.repository;
 
-import com.codecozy.server.entity.BookReview;
 import com.codecozy.server.entity.BookReviewReaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookReviewReactionRepository extends JpaRepository<BookReviewReaction, Long> {
-    BookReviewReaction findByBookReview(BookReview bookReview);
 }
