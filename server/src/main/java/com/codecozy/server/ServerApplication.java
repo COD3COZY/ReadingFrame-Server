4b43573787f50bd2cb06638caa8baadbb173ffad
bee-p <weileenw@naver.com>
2025-03-13 20:31:02 +0900

    remove: 캐시 관련 설정 삭제

--- server/src/main/java/com/codecozy/server/ServerApplication.java
+++ server/src/main/java/com/codecozy/server/ServerApplication.java
@@ -3,10 +3,8 @@ package com.codecozy.server;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class ServerApplication {
 
 	public static void main(String[] args) {
